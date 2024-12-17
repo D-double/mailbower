de8b683ed2498ddf3e2f5b115d2dea246f0cf4bc
Abdulla <dream05@mail.ru>
2024-12-17 07:42:50 +0500

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: 'react', // указывает на глобальную переменную React
+      'react-dom': 'react-dom' // указывает на глобальную переменную ReactDOM
+    }
+  },
   build: {
     lib: {
       entry: 'src/main.jsx',
